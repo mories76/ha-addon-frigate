a34277a63cf987bca9a18767fc13f4e6cf473c28
mories76 <m.kok@mories.com>
2020-03-05 17:48:52 +0100

    Changed python3 to python3.7

--- frigate/run.sh
+++ frigate/run.sh
@@ -3,4 +3,4 @@
 export FRIGATE_RTSP_PASSWORD=$(bashio::config 'FRIGATE_RTSP_PASSWORD')
 export CONFIG_FILE=$(bashio::config 'CONFIG_FILE')
 
-python3 -u detect_objects.py
+python3.7 -u detect_objects.py
